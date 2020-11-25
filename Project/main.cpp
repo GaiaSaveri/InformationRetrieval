4b73e2e585f78748e0062db977570e3b0a4875a6
Gaia Saveri <gaia.saveri@gmail.com>
2020-11-25 12:46:04 +0100

    added dictionary compression

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,47 +1,32 @@
-#include"query_parser.hpp"
-//#include<sys/mman.h>
-//#include<sys/types.h>
-//#include<fcntl.h>
-//#include<unistd.h>
-
-#include<cstdlib>
-#include<ctime>
+#include"dictionary_compression.hpp"
+#include"B+Tree/linkedlist.hpp"
 
 int main(){
+  std::string dictName = "dictionary.txt";
+  std::string compName = "compressed_dictionary.txt";
+  std::string postName = "posting_lists.txt";
+  remove("compressed_dictionary.txt");
+  //starting byte of each block
+  std::vector<int> offsets;
+  int lastBlock;
+  compressDictionary(dictName, compName, 4, offsets, lastBlock);
+  //ptr to the beginning of compressed dictionary
+  unsigned char* cdptr = fileToDisk<unsigned char>(compName);
+  std::string term = "moslem";
+  //int b = findBlock(term, cdptr, offsets);
+  //std::cout<<b<<std::endl;
+  std::pair<int, int> pair;
+  int t = findTerm(term, cdptr, offsets, pair, 4, lastBlock);
+  std::cout<<"found? "<< t <<std::endl;
+  std::cout<<pair.first<<" "<<pair.second<<std::endl;
+  //line is ((first*k)+second)+1
 
   /**
-  std::vector<std::string> filenames;
-
-  Index i{filenames};
-
-  i.printInvertedIndex();
-  */
-  std::string dictionary = "dictionary.txt";
-  std::string posting_lists = "posting_lists.txt";
-  Index ii{dictionary, posting_lists};
-  //ii.printInvertedIndex();
-  //i.saveIndex();
-
-  std::string query = "(maintain AND hold) OR (pct AND year)";
-
-   List<int> r = answer(query, ii);
-   std::cout<<"Query result is: "<<r<<std::endl;
-
-
-
-  //for(auto x : tokens) std::cout<<x<<" ";
-  //std::cout<<std::endl;
-  //for(auto y : currentQuery) std::cout<<y<<" ";
-  //std::cout<<std::endl;
-
-
-  //the first subexpression is surely a simple query
-
-
-  //no parenthesis case-->find terms and operators
-
-   //List<int> result;
-   //answerSimpleQuery(tokens, result, ii);
-   //std::cout<<result<<std::endl;
-   //----------------------ok no parentheses--------------------//
+  for(int i=0; i<offsets.size(); i++){
+    std::vector<std::string> termBlock;
+    readBlockCompressed(cdptr, 4, termBlock);
+    for(auto x : termBlock) std::cout<<x<<std::endl;
+    termBlock.clear();
   }
+  */
+}
